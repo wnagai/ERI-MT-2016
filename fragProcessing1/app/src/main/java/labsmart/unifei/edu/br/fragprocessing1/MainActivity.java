5ef40ff380cf9f6e4c5be6026227538a4c8040a0
Walter Aoiama Nagai <wnagai@gmail.com>
2016-10-17 23:10:54 -0200

    First example using Android Studio and Processing

---
+++ fragProcessing1/app/src/main/java/labsmart/unifei/edu/br/fragprocessing1/MainActivity.java
@@ -0,0 +1,21 @@
+package labsmart.unifei.edu.br.fragprocessing1;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new Sketch();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+}
