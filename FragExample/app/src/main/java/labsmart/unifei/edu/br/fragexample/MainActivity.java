b16f10100f52772c2058902d21991aa289460f2b
Walter Aoiama Nagai <wnagai@gmail.com>
2016-11-10 06:07:39 -0200

    Menu options

--- FragExample/app/src/main/java/labsmart/unifei/edu/br/fragexample/MainActivity.java
+++ FragExample/app/src/main/java/labsmart/unifei/edu/br/fragexample/MainActivity.java
@@ -4,8 +4,10 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import static processing.core.PGraphics.R;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +16,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
         FragmentManager fragmentManager = getFragmentManager();
         Fragment fragment = new ColorfulBalls();
         fragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
